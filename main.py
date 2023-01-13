38797b9dcf659a801c13f6946eacb9fdc4de274e
Radioaktyv <lukaszxmurdzia@gmail.com>
2023-01-13 17:01:55 +0100

    Created base structure.

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import sqlite3
+import shutil
+from time import time
+
+
+def copyfile(src, dst, text):
+    shutil.copy(src, dst)
+    print(text)
+
+
+copyfile("Reviews/database.sqlite", "Backup/database.sqlite", "Backup created.")
+
 con = sqlite3.connect("Reviews/database.sqlite")
+
+copyfile("Backup/database.sqlite", "Reviews/database.sqlite", "Backup restored.")

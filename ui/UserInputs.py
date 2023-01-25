1d73032e3339fabf2aededd307dca80a4c685596
Kamil Niemiec <ksonax@gmail.com>
2023-01-25 15:49:44 +0100

    Added basic UI

---
+++ ui/UserInputs.py
@@ -0,0 +1,12 @@
+class UserInputs:
+
+    def __init__(
+            self,
+            records_amount,
+            selected_database,
+            crud_method
+
+    ):
+        self.records_amount = records_amount
+        self.selected_database = selected_database
+        self.crud_method = crud_method
---
+++ ui/UserInterface.py
@@ -0,0 +1,145 @@
+import sqlite3
+
+import customtkinter
+
+from ui.UserInputs import UserInputs
+from time import time
+from tkinter import messagebox
+import tkinter as tk
+from functions.handleFiles import copyfile
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+
+
+class UserInterface(customtkinter.CTk):
+    WIDTH = 640
+    HEIGHT = 320
+
+    def __init__(self):
+        super().__init__()
+        self.timer = 0
+        self.title("Genetic Algorithm for finding MIN/MAX in Beale Function")
+        self.geometry(f"{UserInterface.WIDTH}x{UserInterface.HEIGHT}")
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)  # call .on_closing() when app gets closed
+        #self.photo_image_sqlite = tk.PhotoImage(file='./img/sqlite.png')
+        #self.photo_image_mongodb = tk.PhotoImage(file='./img/mongodb.png')
+
+        # ============ create two frames ============
+
+        # configure grid layout (2x1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.frame_left = customtkinter.CTkFrame(master=self,
+                                                 width=180,
+                                                 corner_radius=0)
+        self.frame_left.grid(row=0, column=0, sticky="nswe")
+
+        self.frame_right = customtkinter.CTkFrame(master=self)
+        self.frame_right.grid(row=0, column=1, sticky="nswe", padx=20, pady=20)
+
+        # ============ frame_left ============
+
+        # configure grid layout (1x11)
+        self.frame_left.grid_rowconfigure(0, minsize=10)  # empty row with minsize as spacing
+        self.frame_left.grid_rowconfigure(5, weight=1)  # empty row as spacing
+        self.frame_left.grid_rowconfigure(8, minsize=20)  # empty row with minsize as spacing
+        self.frame_left.grid_rowconfigure(11, minsize=10)  # empty row with minsize as spacing
+
+        self.label_1 = customtkinter.CTkLabel(master=self.frame_left,
+                                              text="Beale Function")  # font name and size in px
+        self.label_1.grid(row=1, column=0, pady=10, padx=10)
+
+        self.button_1 = customtkinter.CTkButton(master=self.frame_left,
+                                                text="Plot Function",
+                                                command=self.plot_button_event)
+        self.button_1.grid(row=2, column=0, pady=10, padx=20)
+
+        self.label_mode = customtkinter.CTkLabel(master=self.frame_left, text="Appearance Mode:")
+        self.label_mode.grid(row=9, column=0, pady=0, padx=20, sticky="w")
+
+        self.optionmenu_1 = customtkinter.CTkOptionMenu(master=self.frame_left,
+                                                        values=["Light", "Dark", "System"],
+                                                        command=self.change_appearance_mode)
+        self.optionmenu_1.grid(row=10, column=0, pady=25, padx=20, sticky="w")
+
+        # ============ frame_right ============
+
+        # configure grid layout (3x11)
+        self.frame_right.rowconfigure((0, 1, 2, 3), weight=2)
+        self.frame_right.rowconfigure(10, weight=10)
+        self.frame_right.columnconfigure((0, 1), weight=1)
+        self.frame_right.columnconfigure(2, weight=0)
+
+        # ============ frame_info ============
+
+        # ============ frame_right ============
+
+        self.label_radio_group = customtkinter.CTkLabel(master=self.frame_right,
+                                                        text="Choose Options:")
+        self.label_radio_group.grid(row=0, column=0, columnspan=2, pady=15, padx=15, sticky="")
+
+        self.combobox_selected_db = customtkinter.CTkComboBox(master=self.frame_right,
+                                                              values=["SQLite", "MongoDb"])
+        self.combobox_selected_db.grid(row=1, column=0, columnspan=2, pady=15, padx=15, sticky="we")
+
+        self.combobox_crud_method = customtkinter.CTkComboBox(master=self.frame_right,
+                                                              values=["CREATE", "READ", "UPDATE", "DELETE"])
+        self.combobox_crud_method.grid(row=2, column=0, columnspan=2, pady=15, padx=15, sticky="we")
+
+        self.entry_records_ammount = customtkinter.CTkEntry(master=self.frame_right,
+                                                            width=120,
+                                                            placeholder_text="Records amount")
+        self.entry_records_ammount.grid(row=0, column=0, columnspan=2, pady=15, padx=15, sticky="we")
+
+        self.button_start = customtkinter.CTkButton(master=self.frame_right,
+                                                    border_width=2,  # <- custom border_width
+                                                    fg_color="grey",  # <- no fg_color
+                                                    command=self.button_start,
+                                                    text="Start")
+        self.button_start.grid(row=10, column=0, columnspan=3, pady=15, padx=15, sticky="swe")
+
+        # set default values
+        self.combobox_selected_db.set("Select Database")
+        self.combobox_crud_method.set("CRUD Operation")
+
+    def button_start(self):
+        x = self.get_user_inputs()
+        time_start = time()
+        # Na początku stworzyć backup
+        copyfile("Reviews/database.sqlite", "Backup/database.sqlite", "Backup created.")
+
+        # wczytac ui
+        # polączyć z sql
+        con = sqlite3.connect("Reviews/database.sqlite")
+        # polączyć z mongo
+        # wykonać polecenia zgodnie z ui
+        # obliczyć czasy
+        # wykonać wykresy porównawcze
+
+        # Na koniec przywrócić backup
+        copyfile("Backup/database.sqlite", "Reviews/database.sqlite", "Backup restored.")
+        time_end = time()
+        time_diff = time_end - time_start
+        result = "Execution in sec: " + str(round(time_diff, 5))
+        messagebox.showinfo("output",  result)
+
+
+
+    @staticmethod
+    def change_appearance_mode(new_appearance_mode):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+    def on_closing(self, event=0):
+        self.destroy()
+
+    @staticmethod
+    def plot_button_event():
+        print("button")
+
+    def get_user_inputs(self):
+        return UserInputs(
+            int(self.entry_records_ammount.get()),
+            self.combobox_selected_db.get(),
+            self.combobox_crud_method.get()
+        )
